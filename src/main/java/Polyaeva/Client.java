054286809ce3e5851b3140655a5f3e9105c5bf71
Liubov P. JD-41 <lovely8419@mail.ru>
2023-11-11 15:42:51 +0000

    Initial commit

---
+++ src/main/java/Polyaeva/Client.java
@@ -0,0 +1,26 @@
+package Polyaeva;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        String host = "localhost";
+        int port = 80;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new
+                     PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new
+                     InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Liubov Polyaeva");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+
+    }
+}
