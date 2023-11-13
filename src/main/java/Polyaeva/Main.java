c89741fc26171d3a7ed6e7dcbff0e14d0bf231a3
Liubov P. JD-41 <lovely8419@mail.ru>
2023-11-13 15:52:58 +0000

    update

--- src/main/java/Polyaeva/Main.java
+++ src/main/java/Polyaeva/Main.java
@@ -9,17 +9,25 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.println("Server started");
         int port = 80;
-        ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-        Socket clientSocket = serverSocket.accept(); // ждем подключения
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+        try (ServerSocket serverSocket = new ServerSocket(port)) { // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                ) {
 
-        System.out.println("New connection accepted");
+                    System.out.println("New connection accepted. Port:%d%n\", clientSocket.getPort()");
 
-        final String name = in.readLine();
+                    final String name = in.readLine();
 
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
